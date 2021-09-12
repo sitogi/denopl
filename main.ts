c6ccdcd274d2c441779346509130e066eb54c229
sitogi <takano162@gmail.com>
2021-09-12 14:00:37 +0900

    Add error handling

--- main.ts
+++ main.ts
@@ -1,38 +1,50 @@
 import { parse } from "https://deno.land/std@0.106.0/flags/mod.ts";
 
-const parsedArgs = parse(Deno.args);
-if (!parsedArgs.m) {
-  console.error("-m is not specified!");
+try {
+  const parsedArgs = parse(Deno.args);
+  if (!parsedArgs.m) {
+    throw new Error("-m is not specified!");
+  }
+
+  const apiKey = Deno.env.get("DEEPL_API_KEY");
+  if (!apiKey) {
+    throw new Error("environment variable DEEPL_API_KEY is not specified!");
+  }
+
+  const params = new URLSearchParams({
+    auth_key: apiKey,
+    text: parsedArgs.m,
+    target_lang: parsedArgs.t ?? "EN",
+  });
+
+  // TODO: クエリパラメータ指定じゃなくてちゃんと Body でできないの？ 漏洩するけど
+  const url = `https://api-free.deepl.com/v2/translate`;
+  const res = await fetch(`${url}?${params}`);
+
+  const json = await res.json();
+  const translated = json.translations[0].text;
+
+  const outputForAlfred = {
+    items: [
+      {
+        arg: translated,
+        title: translated,
+        subtitle: translated,
+      },
+    ],
+  };
+  console.log(JSON.stringify(outputForAlfred));
+} catch (e) {
+  if (e instanceof Error) {
+    const outputForAlfred = {
+      items: [{ title: "Error", subtitle: String(e) }],
+    };
+    console.log(JSON.stringify(outputForAlfred));
+  } else {
+    const outputForAlfred = {
+      items: [{ title: "Error", subtitle: "unexpected error" }],
+    };
+    console.log(JSON.stringify(outputForAlfred));
+  }
   Deno.exit(1);
 }
-
-const apiKey = Deno.env.get("DEEPL_API_KEY");
-if (!apiKey) {
-  console.error("environment variable DEEPL_API_KEY is not specified!");
-  Deno.exit(1);
-}
-
-const params = new URLSearchParams({
-  auth_key: apiKey,
-  text: parsedArgs.m,
-  target_lang: parsedArgs.t ?? "EN",
-});
-
-// TODO: クエリパラメータ指定じゃなくてちゃんと Body でできないの？ 漏洩するけど
-const url = `https://api-free.deepl.com/v2/translate`;
-const res = await fetch(`${url}?${params}`);
-
-const json = await res.json();
-const translated = json.translations[0].text;
-
-const outputForAlfred = {
-  items: [
-    {
-      arg: translated,
-      title: translated,
-      subtitle: translated,
-    },
-  ],
-};
-
-console.log(JSON.stringify(outputForAlfred));
